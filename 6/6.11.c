db8ebb00f5d192c1ab0ded22ebdd366fc57d82ce
iBug <7273074+iBug@users.noreply.github.com>
2017-11-07 22:20:06 +0800

    Add 6.11

---
+++ 6/6.11.c
@@ -0,0 +1,19 @@
+// File: 6.11.c
+// Author: iBug
+
+
+#include <stdio.h>
+
+int main(){
+	int i, j;
+	for (i = 0; i < 5; i ++){
+		for (j = 0; j < i; j ++){
+			printf("  ");
+		}
+		for (j = 0; j < 5; j ++){
+			printf("* ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
