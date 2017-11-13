f4fc27ebb515b71795076de5335b1e880a536111
iBug <7273074+iBug@users.noreply.github.com>
2017-11-13 21:45:36 +0800

    Update 7.7

--- 7/7.7.c
+++ 7/7.7.c
@@ -4,12 +4,12 @@
 #include <stdio.h>
 
 int vowelCopy(char *dest, const char *src){
-	int n = 0;
-	for (; src != '\0'; src ++){
-		switch (*src){
+	int n = 0, m;
+	for (m = 0; src[m] != '\0'; m ++){
+		switch (src[m]){
 			case 'A': case 'E': case 'I': case 'O': case 'U':
 			case 'a': case 'e': case 'i': case 'o': case 'u':
-				dest[n] = *src;
+				dest[n] = src[m];
 				n ++;
 				break;
 		}
