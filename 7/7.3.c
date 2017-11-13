e7bdf898397b89ca7874951d3f0a226ad4d29204
iBug <7273074+iBug@users.noreply.github.com>
2017-11-13 19:12:41 +0800

    Add 7.3

---
+++ 7/7.3.c
@@ -0,0 +1,26 @@
+// File: 7.3.c
+// Author: iBug
+
+#include <stdio.h>
+
+int isPrime(int n){
+	int i;
+	for (i = 2; i*i <= n; i ++){
+		if (n % i == 0)
+			return 0;
+	}
+	return n;
+}
+
+int main(){
+	int n;
+	printf("Please give a number: ");
+	scanf("%d", &n);
+	if (isPrime(n)){
+		printf("%d is a prime number.\n");
+	}
+	else{
+		printf("%d is not a prime number.\n");
+	}
+	return 0;
+}
