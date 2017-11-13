a2173a4086af08d07d4819b8445fceb9c60cdd57
iBug <7273074+iBug@users.noreply.github.com>
2017-11-13 19:17:43 +0800

    Add 7.1

---
+++ 7/7.1.c
@@ -0,0 +1,31 @@
+// File: 7.1.c
+// Author: iBug
+
+#include <stdio.h>
+
+int gcd(int m, int n){
+	int t;
+	if (m < n){
+		t = m;
+		m = n;
+		n = t;
+	}
+	while (n != 0){
+		t = m % n;
+		m = n;
+		n = t;
+	}
+	return m;
+}
+
+int lcm(int m, int n){
+	return m * n * gcd(m, n);
+}
+
+int main(){
+	int a, b;
+	printf("Please give two numbers: ");
+	scanf("%d%d", &a, &b);
+	printf("Their gcd is %d. Their lcm is %d.\n", gcd(a, b), lcm(a, b));
+	return 0;
+}
