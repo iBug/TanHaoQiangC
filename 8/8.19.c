cb047cf649244b15a9b5df08857ed38603c89d24
iBug <7273074+iBug@users.noreply.github.com>
2017-11-17 13:33:27 +0800

    Add 8.19

---
+++ 8/8.19.c
@@ -0,0 +1,16 @@
+// File: 8.19.c
+// Author: iBug
+
+#include <stdio.h>
+
+int main(){
+	puts("*************");
+	puts("*           *");
+	puts("*  WARNING  *");
+	puts("*           *"):
+	puts("*************");
+	puts("");
+	puts("Exercise 8.19 is EXTREMELY STUPID.");
+	puts("I refuse to do it!");
+	return 1;
+}
